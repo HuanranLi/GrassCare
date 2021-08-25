208987b556d53a2e16edb21fc5f72c371039d908
Huanran Li <nemo0421@gmail.com>
2021-08-25 10:08:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='GrassCare-Plot',
+    version='1.0.0',
+    url='https://github.com/HuanranLi/GrassCare-Plot',
+    author='Huanran Li',
+    author_email='hli488@wisc.edu',
+    description='',
+    packages=find_packages(),    
+    install_requires=['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
