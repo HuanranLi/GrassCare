5a42ee86b97d996967ab62d53e89d4127b7cea7b
Huanran Li <nemo0421@gmail.com>
2023-06-01 12:26:53 -0500

    Add Adam

--- src/GradientDescent.py
+++ src/GradientDescent.py
@@ -5,20 +5,20 @@ from ProbMatrix import *
 from Loss import *
 
 '''
-def retraction_GD(b_array, del_L_array, eta, moment, old_del, eps = 1e-5):  
+def retraction_GD(b_array, del_L_array, eta, moment, old_del, eps = 1e-5):
     new_b_array = np.zeros(b_array.shape)
     K = b_array.shape[0]
-   
+
 
     for i in range(K):
         #gradient step
-        
+
         #projectee = b_array[i] - eta * (1 - b_array[i].T @ b_array[i])**2 / 4 * del_L_array[i]
         new_change = eta * (1 - b_array[i].T @ b_array[i])**2 / 4 * del_L_array[i] + moment * (eta * (1 - b_array[i].T @ b_array[i])**2 / 4 * old_del[i])
-        
+
         new_b_array[i] = b_array[i] - new_change
-            
-            
+
+
     return new_b_array
 '''
 
@@ -28,5 +28,22 @@ def retraction_GD(b_array, del_L_array, eta, moment, old_del, eps=1e-5):
 
     new_change = eta * (1 - np.square(b_array).sum(axis=1, keepdims=True)) ** 2 / 4 * del_L_array + moment * (eta * (1 - np.square(b_array).sum(axis=1, keepdims=True)) ** 2 / 4 * old_del)
     new_b_array = b_array - new_change
+
+    return new_b_array
+
+
+def retraction_ADAM(b_array, del_L_array, alpha, beta1, beta2, eps=1e-8):
+    K = b_array.shape[0]
+    m = np.zeros_like(b_array)
+    v = np.zeros_like(b_array)
+    t = 0
+
+    # ADAM update
+    t += 1
+    m = beta1 * m + (1 - beta1) * del_L_array
+    v = beta2 * v + (1 - beta2) * np.square(del_L_array)
+    m_hat = m / (1 - beta1 ** t)
+    v_hat = v / (1 - beta2 ** t)
+    new_b_array = b_array - alpha * m_hat / (np.sqrt(v_hat) + eps)
 
     return new_b_array
--- src/Grasscare.py
+++ src/Grasscare.py
@@ -24,9 +24,9 @@ def b_array_init(count):
     return np.array([x,y]).T
 
 
-        
-        
-def grasscare(U_array, eta = 1, moment = 0.9, max_iter = 400, init = 'random', beta = 2):
+
+
+def grasscare(U_array, gradient_method = 'MomentumGD', eta = 1, moment = 0.9, max_iter = 400, init = 'random', beta = 2):
     #random init Poincare Embedding
     if init == 'random':
         b_array = b_array_init(U_array.shape[0])
@@ -34,42 +34,50 @@ def grasscare(U_array, eta = 1, moment = 0.9, max_iter = 400, init = 'random', b
         b_array = U_array[:,:2,0] * 0.95
     else:
         assert False
-    
+
     # Calculate Probability Matrix in Grassmanian
     P_Gr_mat = P_Gr(U_array = U_array)
-    
-    
+
+
     #Init momentum with 0
     old_del = np.zeros(b_array.shape)
-    
+
     #record all the training history
     history = []
-    
+
     for iter in range(max_iter):
-           
+
         # Calcuate Probability Matrix in Poincare Disk
         dist_b_array_mat = cdist(b_array, b_array, metric=d_p)
         P_Ball_mat, _ = P_Ball(b_array, dist_b_array_mat, beta = beta)
-        
+
         #Calculate Loss and Gradient
         Loss = L_obj(P_Ball_mat, P_Gr_mat)
-        del_L_array = del_L(b_array, 
-                            P_Ball_mat, 
-                            P_Gr_mat, 
+        del_L_array = del_L(b_array,
+                            P_Ball_mat,
+                            P_Gr_mat,
                             dist_b_array_mat)
-        
-        new_b_array = retraction_GD(b_array, 
-                                    del_L_array, 
-                                    eta, 
-                                    moment, 
-                                    old_del)
+
+        if gradient_method = 'MomentumGD':
+            new_b_array = retraction_GD(b_array,
+                                        del_L_array,
+                                        eta,
+                                        moment,
+                                        old_del)
+        elif gradient_method = 'ADAM':
+            new_b_array = retraction_ADAM(b_array,
+                                        del_L_array,
+                                        eta,
+                                        beta1 = 0.5,
+                                        beta2 = 0.7,
+
+
         old_del =  del_L_array
         b_array = new_b_array
-        
+
         record = {'iter': iter, 'obj': Loss, 'b_array': b_array}
         history.append(record)
-        
+
         print(iter, Loss)
-        
+
     return history
-    
